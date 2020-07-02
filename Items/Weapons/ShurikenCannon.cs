3aa7fccfa873a047ea1c5aff585e4b997913095e
Kyle Berndt <polling13@gmail.com>
2020-07-02 17:37:32 -0500

    shuriken cannon art & attempted ninja synergy

--- Items/Weapons/ShurikenCannon.cs
+++ Items/Weapons/ShurikenCannon.cs
@@ -1,3 +1,4 @@
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -16,7 +17,7 @@ namespace Desolation.Items.Weapons
             item.ranged = true;
             item.noMelee = true;
             item.autoReuse = true;
-            item.damage = 15;
+            item.damage = 16;
             item.shootSpeed = 11.6f;
             item.useTime = 14;
             item.useAnimation = 14;
@@ -25,8 +26,8 @@ namespace Desolation.Items.Weapons
             // Cosmetic
             item.useStyle = ItemUseStyleID.HoldingOut;
             item.UseSound = SoundID.Item11;
-            item.width = 17;
-            item.height = 33;
+            item.width = 27;
+            item.height = 12;
 
             // Ammo
             item.useAmmo = ItemID.Shuriken;
@@ -35,6 +36,21 @@ namespace Desolation.Items.Weapons
             // Misc
             item.value = 10000;
             item.rare = ItemRarityID.Green;
+        }
+
+        public override bool ConsumeAmmo(Player player)
+        {
+            return !(player.head == ItemID.NinjaHood && player.body == ItemID.NinjaShirt && player.legs == ItemID.NinjaPants) && base.ConsumeAmmo(player);
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddTile(TileID.Anvils);
+            recipe.AddIngredient(ItemID.IllegalGunParts);
+            recipe.AddIngredient(ItemID.Shuriken, 50);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
         }
     }
 }
