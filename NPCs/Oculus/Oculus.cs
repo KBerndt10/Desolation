201e42d285b53132c90fb63519712ddab0d65783
Kyle Berndt <polling13@gmail.com>
2020-08-10 17:15:42 -0500

    Initial commit for Oculus

---
+++ NPCs/Oculus/Oculus.cs
@@ -0,0 +1,103 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Desolation.NPCs.Oculus
+{
+    internal class Oculus : ModNPC
+    {
+        private int myFrame = 0;
+
+        public override void SetStaticDefaults()
+        {
+            //Main.npcFrameCount[npc.type] = 8;
+        }
+
+        public override void SetDefaults()
+        {
+            npc.value = 5000f;
+
+            // Cosmetic
+            npc.width = 194;
+            npc.height = 128;
+            animationType = -1;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath2;
+            npc.alpha = 0;
+
+            // AI
+            npc.aiStyle = -1;
+            aiType = -1;
+            npc.boss = true;
+
+            // Combat
+            npc.damage = 42;
+            npc.defense = 25;
+            npc.lifeMax = 35000;
+            npc.knockBackResist = 0f;
+
+            npc.noGravity = true;
+
+            // Keep the body untargettable and health bar-free.
+            npc.dontTakeDamage = true;
+        }
+
+        // Which state in the npc.ai[] is which
+
+        private const int AI_State_Slot = 0;
+        private const int AI_Timer_Slot = 1;
+        private const int AI_Slot_2 = 2;
+        private const int AI_Slot_3 = 3;
+
+        // State values
+        private enum State
+        {
+            Initial = 0, Waiting
+        }
+
+        private const float speed = 8f;
+        private const float acceleration = 1.2f;
+
+        // Getters and Setters for AI slots for convenience
+        public float AI_State
+        {
+            get => npc.ai[AI_State_Slot];
+            set => npc.ai[AI_State_Slot] = value;
+        }
+
+        public float AI_Timer
+        {
+            get => npc.ai[AI_Timer_Slot];
+            set => npc.ai[AI_Timer_Slot] = value;
+        }
+
+        private Player Target
+        {
+            get => Main.player[npc.target];
+            set => npc.target = value.whoAmI;
+        }
+
+        public float Speed
+        {
+            get => npc.velocity.Length();
+            set { npc.velocity.Normalize(); npc.velocity *= value; }
+        }
+
+        private int PrimaryEyeID = -2;
+
+        private void SpawnEyes()
+        {
+            PrimaryEyeID = NPC.NewNPC((int)npc.position.X, (int)npc.position.Y, NPCType<OculusPrimaryEye>(), 0, npc.whoAmI);
+        }
+
+        public override void AI()
+        {
+            if (AI_State == (int)State.Initial)
+            {
+                SpawnEyes();
+                AI_State = (int)State.Waiting;
+            }
+        }
+    }
+}
---
+++ NPCs/Oculus/OculusPrimaryEye.cs
@@ -0,0 +1,106 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Desolation.NPCs.Oculus
+{
+    internal class OculusPrimaryEye : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[npc.type] = 14;
+        }
+
+        public override void SetDefaults()
+        {
+            npc.value = 5000f;
+
+            // Cosmetic
+            npc.width = 36;
+            npc.height = 36;
+            animationType = -1;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath2;
+            npc.alpha = 0;
+
+            // AI
+            npc.aiStyle = -1;
+            aiType = -1;
+            npc.boss = true;
+
+            // Combat
+            npc.damage = 30;
+            npc.defense = 30;
+            npc.lifeMax = 12000;
+            npc.knockBackResist = 0f;
+
+            npc.noGravity = true;
+        }
+
+        // Which state in the npc.ai[] is which
+
+        private const int AI_Master_Slot = 0;
+        private const int AI_State_Slot = 1;
+        private const int AI_Timer_Slot = 2;
+        private const int AI_Slot_3 = 3;
+
+        // State values
+        private enum State
+        {
+            Waiting,
+        }
+
+        private const float speed = 8f;
+        private const float acceleration = 1.2f;
+
+        // Getters and Setters for AI slots for convenience
+        public int AI_Master
+        {
+            get => (int)npc.ai[AI_Master_Slot];
+            set => npc.ai[AI_Master_Slot] = value;
+        }
+
+        public float AI_State
+        {
+            get => npc.ai[AI_State_Slot];
+            set => npc.ai[AI_State_Slot] = value;
+        }
+
+        public float AI_Timer
+        {
+            get => npc.ai[AI_Timer_Slot];
+            set => npc.ai[AI_Timer_Slot] = value;
+        }
+
+        private Player Target
+        {
+            get => Main.player[npc.target];
+            set => npc.target = value.whoAmI;
+        }
+
+        public float Speed
+        {
+            get => npc.velocity.Length();
+            set { npc.velocity.Normalize(); npc.velocity *= value; }
+        }
+
+        private NPC master => Main.npc[AI_Master];
+
+        public override void AI()
+        {
+            // Get rid of this is the master is gone
+            if (!master.active || master.type != NPCType<Oculus>())
+            {
+                npc.active = false;
+            }
+
+            // If over half life, stick to master
+            if (npc.life > npc.lifeMax / 2)
+            {
+                npc.position = master.Center;
+                npc.TargetClosest();
+            }
+        }
+    }
+}
