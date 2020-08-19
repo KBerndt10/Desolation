019b38ac50d6990fd451fc9a013d7b6ae3453b86
Kyle Berndt <polling13@gmail.com>
2020-08-19 16:47:59 -0500

    Fiinshing touches for circlebook projectile

--- NPCs/Librarian/Projectiles/CircleBook.cs
+++ NPCs/Librarian/Projectiles/CircleBook.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using Terraria;
 using Terraria.ID;
@@ -11,16 +12,31 @@ namespace Desolation.NPCs.Librarian.Projectiles
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Tome of Forbidden Knowledge");
+            Main.projFrames[projectile.type] = 2;
         }
 
         public override void SetDefaults()
         {
-            projectile.width = 16;
-            projectile.height = 16;
+            projectile.width = 32;
+            projectile.height = 32;
             projectile.friendly = false;
             projectile.hostile = true;
             projectile.tileCollide = false;
             projectile.timeLeft = 840;
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            if (circle)
+            {
+                projectile.frame = 0;
+            }
+            else
+            {
+                projectile.frame = 1;
+                projectile.rotation += projectile.direction * 0.03f;
+            }
+            return base.PreDraw(spriteBatch, lightColor);
         }
 
         protected float AI_0
@@ -56,8 +72,8 @@ namespace Desolation.NPCs.Librarian.Projectiles
                 if (Main.netMode != NetmodeID.MultiplayerClient)
                 {
                     float rads = MathHelper.ToRadians(upTime);
-                    projectile.position.X += 3 * (float)Math.Cos(rads);
-                    projectile.position.Y += 3 * (float)Math.Sin(rads);
+                    Vector2 dest = new Vector2(projectile.position.X + 3 * (float)Math.Cos(rads), projectile.position.Y + 3 * (float)Math.Sin(rads));
+                    projectile.velocity = dest - projectile.position;
                     projectile.netUpdate = true;
                 }
             }
